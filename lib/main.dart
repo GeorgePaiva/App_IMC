ac3698ea452625a46bc45120f48db760527b2260
George Paiva <george.paiva15@gmail.com>
2019-09-28 00:23:21 -0300

    add_controller

--- lib/main.dart
+++ lib/main.dart
@@ -12,17 +12,50 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+
+  String _infoText = "Informe seus dados!";
+
+  void _resetField() {
+    weightController.text = "";
+    heightController.text = "";
+    _infoText = "Informe seus dados!";
+  }
+
+  void _calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+      print(imc);
+      if (imc < 18.6) {
+        _infoText = "Abaixo do Peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = "Peso Ideal (${imc.toStringAsPrecision(3)})";
+      }else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Levemente Acima do Peso (${imc.toStringAsPrecision(3)})";
+      }else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obesidade Grau I(${imc.toStringAsPrecision(3)})";
+      }else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = "Obesidade Grau II(${imc.toStringAsPrecision(3)})";
+      }else if (imc >= 40) {
+        _infoText = "Obesidade Grau III(${imc.toStringAsPrecision(3)})";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("Calculadora de IMC"),
           centerTitle: true,
-          backgroundColor: Colors.green,
+          backgroundColor: Colors.deepPurple[300],
           actions: <Widget>[
             IconButton(
               icon: Icon(Icons.refresh),
-              onPressed: () {},
+              onPressed: _resetField,
             )
           ],
         ),
@@ -32,41 +65,44 @@ class _HomeState extends State<Home> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+              Icon(Icons.account_circle,
+                  size: 120.0, color: Colors.deepPurple[300]),
               TextField(
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
                     labelText: "Peso (Kg)",
-                    labelStyle: TextStyle(color: Colors.green)),
+                    labelStyle: TextStyle(color: Colors.deepPurple[300])),
                 textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.green, fontSize: 25.0),
+                style: TextStyle(color: Colors.deepPurple[300], fontSize: 25.0),
+                controller: weightController,
               ),
               TextField(
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
                     labelText: "Altura (cm)",
-                    labelStyle: TextStyle(color: Colors.green)),
+                    labelStyle: TextStyle(color: Colors.deepPurple[300])),
                 textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.green, fontSize: 25.0),
+                style: TextStyle(color: Colors.deepPurple[300], fontSize: 25.0),
+                controller: heightController,
               ),
               Padding(
                 padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
                 child: Container(
                   height: 50.0,
                   child: RaisedButton(
-                    onPressed: () {},
+                    onPressed: _calculate,
                     child: Text(
                       "Calcular",
                       style: TextStyle(color: Colors.white, fontSize: 25.0),
                     ),
-                    color: Colors.green,
+                    color: Colors.deepPurple[300],
                   ),
                 ),
               ),
               Text(
-                "Info",
+                _infoText,
                 textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.green, fontSize: 25.0),
+                style: TextStyle(color: Colors.deepPurple[300], fontSize: 25.0),
               )
             ],
           ),
